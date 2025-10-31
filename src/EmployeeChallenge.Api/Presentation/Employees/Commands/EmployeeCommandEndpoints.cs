d7a85be62f85fe995dc916cc66b1f758bfb4ea45
Andres Descalzo <andresdescalzo@gmail.com>
2025-10-31 00:23:11 -0300

    fix issues on update employee

--- src/EmployeeChallenge.Api/Presentation/Employees/Commands/EmployeeCommandEndpoints.cs
+++ src/EmployeeChallenge.Api/Presentation/Employees/Commands/EmployeeCommandEndpoints.cs
@@ -23,7 +23,10 @@ internal class EmployeeCommandEndpoints : IEndpoint
                 .SendAsync<EmployeeCreateCommand, Result<Guid>>(new EmployeeCreateCommand(payload), ct)
                 .ConfigureAwait(false);
 
-            return result.ToCreatedAtRouteResult(EmployeesConstants.EndpointGetEmployeeByIdName, result.Value);
+            return result.ToCreatedAtRouteResult(
+                EmployeesConstants.EndpointGetEmployeeByIdName,
+                new { id = result.Value }
+            );
         })
         .WithName("CreateEmployee")
         .WithOpenApi()
@@ -43,7 +46,10 @@ internal class EmployeeCommandEndpoints : IEndpoint
                 .SendAsync<EmployeeUpdateCommand, Result<Guid>>(new EmployeeUpdateCommand(id, payload), ct)
                 .ConfigureAwait(false);
 
-            return result.ToCreatedAtRouteResult(EmployeesConstants.EndpointGetEmployeeByIdName, result.Value);
+            return result.ToCreatedAtRouteResult(
+                EmployeesConstants.EndpointGetEmployeeByIdName,
+                new { id = result.Value }
+            );
         })
         .WithName("UpdateEmployee")
         .WithOpenApi()
